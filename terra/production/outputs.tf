97e8fedc21f361a326a8ab0a61cad4c608f2df0d
B-R-H <barnaby66@gmail.com>
2020-04-20 12:53:51 +0100

    Kuberneties teform configured

---
+++ terra/production/outputs.tf
@@ -0,0 +1,6 @@
+output "client_certificate" {
+  value = azurerm_kubernetes_cluster.example.kube_config.0.client_certificate
+}
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.example.kube_config_raw
+}
---
+++ terra/production/varibles.tf
@@ -0,0 +1,5 @@
+variable "vm_size" {default = "Standard_DS1_v2"}
+variable "min_nodes" {default = 1}
+variable "max_nodes" {default = 3}
+variable "client_id" {}
+variable "client_secret" {}
